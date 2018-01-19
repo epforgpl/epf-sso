05c13acd4d9d96aaa260d6c249e3db40631b9801
Jan Zankowski <jan.zankowski+signup@gmail.com>
2018-01-19 17:57:35 +0100

    CORS domains in .env file.

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => [],
+    'allowedOrigins' => explode(',', env('CORS_ALLOWED_ORIGINS')),
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['GET'],
