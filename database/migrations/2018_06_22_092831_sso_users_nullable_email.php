26d63fcbf90fcaad5ffc60365074b4da22090662
Jan Zankowski <jan.zankowski+signup@gmail.com>
2018-06-22 11:35:12 +0200

    When using social login, user may choose not to give us their email address - hence it should be nullable.

---
+++ database/migrations/2018_06_22_092831_sso_users_nullable_email.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Reason for this migration: when using social login (e.g. Facebook), the user may choose not to pass
+ * their email address to our app.
+ */
+class SsoUsersNullableEmail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sso_users', function (Blueprint $table) {
+            $table->string('email', 255)->unique()->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sso_users', function (Blueprint $table) {
+            $table->string('email', 255)->unique()->change();
+        });
+    }
+}
