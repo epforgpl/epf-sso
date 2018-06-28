8d8f345f3d3d6ca4ad342e0d9af5e37c2ca29ce5
Jan Zankowski <jan.zankowski+signup@gmail.com>
2018-06-28 22:21:18 +0200

    Unused

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App\Auth\EpfUserProvider;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
