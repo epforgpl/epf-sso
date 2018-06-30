b40386373fb40e6436599dcc16094b5e92a5e21f
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2018-06-30 16:57:53 +0200

    "Remember me" checkbox on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,6 +24,13 @@
                         @endif
                     </div>
 
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label class="custom-control-label" for="remember">Zapamiętaj mnie</label>
+                        </div>
+                    </div>
+
                 </fieldset>
                 <div class="text-center mt-2">
                     <p class="mb-4">
@@ -33,72 +40,17 @@
                     </p>
                     <p class="mb-1 text-muted"><a href="{{ route('password.request') }}">Nie pamiętasz hasła?</a></p>
                     <p class="mb-0 text-muted"><a href="{{ route('register') }}">Utwórz nowe konto</a></p>
+                    <!--
+                    <button class="btn btn-primary" onclick="location.href = '{{ url('/') }}/oauth/google';">
+                        Zaloguj się przez Google
+                    </button>
+                    <button class="btn btn-primary" onclick="location.href = '{{ url('/') }}/oauth/facebook';">
+                        Zaloguj się przez Facebook
+                    </button>
+                    -->
                 </div>
             </form>
 
         </div>
     </div>
-
-
-<!--
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}" autocomplete="off">
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>Zapamiętaj mnie
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Zaloguj się
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Nie pamiętasz hasła?
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                &nbsp;
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <b>Lub:</b>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button class="btn btn-primary" onclick="location.href = '{{ url('/') }}/oauth/google';">
-                                    Zaloguj się przez Google
-                                </button>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button class="btn btn-primary" onclick="location.href = '{{ url('/') }}/oauth/facebook';">
-                                    Zaloguj się przez Facebook
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
--->
 @endsection
