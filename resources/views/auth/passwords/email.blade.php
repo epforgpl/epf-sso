a441e09eebd51ec3daf6fa4dd572d35463344fab
Jan Zankowski <jan.zankowski+signup@gmail.com>
2018-07-05 11:20:48 +0200

    Better style for `forgotten password` page.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,45 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}" autocomplete="off">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Adres e-mail</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="off">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Wyślij link do zmiany hasła
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="card card-main card-register">
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            <form class="form-horizontal" method="POST" action="{{ route('password.email') }}" autocomplete="off">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="col-md-4 control-label">Adres e-mail</label>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="off">
+                    @if ($errors->has('name'))
+                        <div class="invalid-feedback">{{ $errors->first('email') }}</div>
+                    @endif
+                </div>
+
+                <div class="text-center mt-2">
+                    <p class="mb-4">
+                        <button type="submit" class="btn btn-primary">
+                            Wyślij link do zmiany hasła
+                        </button>
+                    </p>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
